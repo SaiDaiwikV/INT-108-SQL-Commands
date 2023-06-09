39fc85db0ad32dcff66006bb96215ee14340892b
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2023-06-09 12:47:56 +0530

    Add files via upload

---
+++ creating_table.sql
@@ -0,0 +1,10 @@
+
+create table family(id int, fname varchar(50),lname varchar(50));
+
+insert into family values(1,'saidaiwik','vontimitta');
+
+insert into family values(2,'lakshmi','prasad');
+
+insert into family values(3,'sree ram','prasad');
+
+select * from family;
